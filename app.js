bd88de0599d11aae779edbf02c58e37114566791
Guilherme Magalhaes <guiandmag@gmail.com>
2015-02-10 20:26:04 -0200

    feat(config): MongoDB - Connect Connect the API with the MongoDB by Mongoose library Closes #5

--- app.js
+++ app.js
@@ -1,70 +1,50 @@
+"use strict";
+
+// Module dependencies
 var express = require('express'),  
     app = express(),
     path = require('path'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session'),
     bodyParser = require('body-parser'),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+    methodOverride = require('method-override'),
+    flash = require('connect-flash'),
+    errorHandler = require('errorhandler'),
+    routes = require('./routes'),
+    config = require('./config.json'),
+    db = require('./lib/db');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-
-app.use('/client', express.static(__dirname + '/client'));
-app.use('/common', express.static(__dirname + '/common'));
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/client/index.html');
-}); 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// Loading middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  secret: config.sessionSecret,
+  cookie: {
+    maxAge: config.sessionMaxAge
+  }
+}));
+app.use(flash());
 
+// Declaring application routes
 app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+  app.use(errorHandler());
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
-
-// Start the server.
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080  
-, ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-
-server.listen(port, ip);
+// Establishing database connection and binding application to specified port
+db.connect();
+app.listen(config.port);
+console.log('listening on port %s', config.port);
